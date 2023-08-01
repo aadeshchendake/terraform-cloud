8edf1c96ebe47ebf8f24c2e02502af2bc8b1c346
aadeshchendake <aadeshc.in@mouritech.com>
2023-08-01 17:47:17 +0530

    updated main.tf file

--- main.tf
+++ main.tf
@@ -1,27 +1,7 @@
-provider "azurerm" {
-  features {
-
+resource "null_resource" "terraform_cloud_test" {
+  count = 5
+  provisioner "local-exec" {
+    command = "echo 'Test ${count.index}'"
   }
 }
-
-data "azurerm_resource_group" "example" {
-  name     = var.rg_name
-  #location = var.location
-}
-
-resource "azurerm_service_plan" "example" {
-  name                = var.sp_name
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = data.azurerm_resource_group.example.location
-  os_type             = var.os_type
-  sku_name            = var.sku_name
-}
-
-resource "azurerm_linux_web_app" "example" {
-  name                = var.wapp_name
-  resource_group_name = data.azurerm_resource_group.example.name
-  location            = azurerm_service_plan.example.location
-  service_plan_id     = azurerm_service_plan.example.id
-
-  site_config {}
-}
+  
